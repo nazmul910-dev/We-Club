a4917c2a5527bd5bf9163cf13b63202e7ab8c41b
Musfiqur Rahman <softvancemusfiqur@gmail.com>
2026-07-11 17:04:37 +0600

    seperated the promoters and the listings along with a copy and also implment profile view on each time a user view the profile

--- src/modules/promoters/promoters.routes.ts
+++ src/modules/promoters/promoters.routes.ts
@@ -7,6 +7,7 @@ import { verifyToken } from "../auth/auth.utils";
 const router  = Router();
 
 router.get("/",  promotersController.getPromoters);
+router.patch("/:id/view",  promotersController.incrementPromoterView);
 
 
 export  const promoterRoutes = router;
