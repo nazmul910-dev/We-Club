4ae5ac9afe0f02c218a9a85a93260b3fd7b32ab8
nazmul910-dev <softnazmul48@gmail.com>
2026-08-10 16:23:34 +0600

    fix: update sendCustomMail transporter configuration for compatibility

--- src/utility/sendCustomMail.ts
+++ src/utility/sendCustomMail.ts
@@ -18,11 +18,12 @@ const sendCustomMail = async ({
     host: 'smtp.gmail.com',
     port: 587,
     secure: false,
+    family: 4, 
     auth: {
       user: config.SMTP_AUTH_USER,
       pass: config.SMTP_AUTH_PASS,
     },
-  });
+  } as any);
 
   await transporter.sendMail({
     from: `${config.MAIL_FROM_NAME} <${config.SMTP_AUTH_USER}>`,
