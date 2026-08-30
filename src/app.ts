b37abb076468800ca63dc0f527d70a4b59b42ae9
softvancemusfiqur-web <softvancemusfiqur@gmail.com>
2026-08-30 12:13:40 +0600

    added some more limit on developement

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.set('trust proxy', 1);
 
 const limiter = rateLimit({
  windowMs: 5 * 60 * 1000, // 5 minutes
- limit: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
+ limit: 10000, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
  ipv6Subnet: 60, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
